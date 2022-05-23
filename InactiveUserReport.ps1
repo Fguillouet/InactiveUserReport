38fa81704a981ec32bc52f34be93b6fb46ac2c78
GUILLOUET Franck <franck.guillouet@uni-medias.com>
2022-05-23 17:56:56 +0200

    Barely work
    Création du fichier + script fonctionne

---
+++ InactiveUserReport.ps1
@@ -0,0 +1,20 @@
+#Connexion au Tenant O365
+Connect-MsolService
+Connect-ExchangeOnline
+
+#Récupération de la liste des utilisateurs et filtrage des résultats
+Get-MsolUser | ForEach-Object {
+    $upn = $_.UserPrincipalName
+    $LastLogonTime = (Get-MailboxStatistics -Identity $upn).lastlogontime
+    $DisplayName = $_.DisplayName
+    $IsLicensed = $_.IsLicensed
+    $MBUserCount++
+    Write-Progress -Activity "`n     Processed mailbox count: $MBUserCount "`n"  Currently Processing: $DisplayName"
+
+    [PSCustomObject]@{
+        Name = $DisplayName
+        Mail = $upn
+        LastLogon = $LastLogonTime 
+        IsLicensed = $IsLicensed
+        } | Export-Csv -path "C:\Admin Rapport\Utilisateurs inactifs $(get-date -f dd-MM-yyyy).csv" -notype -Append 
+}
